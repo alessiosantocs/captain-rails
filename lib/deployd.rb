b5a017658ac2539c7912180f9e297c29a356d32e
Salvatore Piazzolla <piazz23@gmail.com>
2014-06-15 21:12:49 +0200

    create gem config system

--- lib/deployd.rb
+++ lib/deployd.rb
@@ -6,4 +6,16 @@ require 'bitbucket_rest_api'
 
 module Deployd
 	require "deployd/railtie.rb" if defined?(Rails)
+
+	@config = Deployd::Config.new
+
+	def self.config
+
+		if block_given?
+			yield(@config)
+		else
+			@config
+		end
+
+	end
 end
---
+++ lib/deployd/config.rb
@@ -0,0 +1,5 @@
+module Deployd
+	class Config
+		attr_accessor :public_key
+	end
+end
