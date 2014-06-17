805c4fb7aa10d14746399609e6c00e746c80a214
Alessio Santo <alessio.santocs@gmail.com>
2014-06-17 22:41:18 +0200

    Added call to api and condition if api returns ok

--- lib/deployd/tasks.rb
+++ lib/deployd/tasks.rb
@@ -14,7 +14,7 @@ namespace :deployd do
 		puts "Deploying Repo(#{repo} at #{branch}) on #{release_env} server heading #{rev}"
 		commit = `git log --format="%H" -n 1`
 
-		uri = URI.parse('http://localhost:4000/api/v1/deployments') # Insert the address of the application api
+		uri = URI.parse('http://localhost:3000/api/v1/deployments') # Insert the address of the application api
 
 		http = Net::HTTP.new(uri.host, uri.port)
 		http.use_ssl = true if use_ssl
@@ -42,20 +42,30 @@ namespace :deployd do
 	task :install, [:public_token] do |t, args|
 		public_token 	= args[:public_token]
 
-		source 			= File.join(File.dirname(__FILE__), '..', '..', 'config', 'initializers', 'deployd.rb')
-      	destination 	= File.new(Rails.root.to_s + '/config/initializers/deployd.rb', 'w')
-
-      	# FileUtils.cp(source, destination)
-
-      	IO.readlines(source.to_s).each do |line|
-      		str = line
-      		str = "\tconfig.token = '#{public_token}'" if str.index('config.token').present?
-
-      		destination.write(str + "\n")
-      	end
-
-      	destination.close
-
       	# call the API
+      	uri = URI.parse("http://localhost:3000/api/v1/deployable_applications/#{public_token}/activate") # Insert the address of the application api
+
+		http = Net::HTTP.new(uri.host, uri.port)
+		# http.use_ssl = true if use_ssl
+
+		request = Net::HTTP::Put.new(uri.path, {'Content-Type' =>'application/json'})
+
+		response = http.request(request)
+		
+		if response.kind_of?(Net::HTTPOK)
+			source 			= File.join(File.dirname(__FILE__), '..', '..', 'config', 'initializers', 'deployd.rb')
+			destination 	= File.new(Rails.root.to_s + '/config/initializers/deployd.rb', 'w')
+
+			# FileUtils.cp(source, destination)
+
+			IO.readlines(source.to_s).each do |line|
+				str = line
+				str = "\tconfig.token = '#{public_token}'" if str.index('config.token').present?
+
+				destination.write(str + "\n")
+			end
+
+			destination.close
+		end
 	end
 end
